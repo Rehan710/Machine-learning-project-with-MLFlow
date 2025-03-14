864bc1ab87c52a46cefb1d44be6a7c88e632cc0d
Rehan710 <rq9195@gmail.com>
2025-03-14 12:44:27 +0530

    Updated main file conflicts

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from mlProject import logger
+from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
 
-logger.info('Welcome to our custom logging')
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:
+    logger.error(f"{STAGE_NAME} failed")
+    logger.error(str(e))
