589b8b9038d19611c9e754c367087c395c7db405
Rehan710 <rq9195@gmail.com>
2025-03-14 12:37:19 +0530

    Added data ingestion pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -11,3 +11,16 @@ try:
 except Exception as e:
     logger.error(f"{STAGE_NAME} failed")
     logger.error(str(e))
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f"Starting {STAGE_NAME}")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f"Completed {STAGE_NAME}")
+except Exception as e:  
+    logger.error(f"{STAGE_NAME} failed")
+    logger.error(str(e))
+    raise e
+    
